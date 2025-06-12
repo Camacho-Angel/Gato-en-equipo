b9c700a97b88b9ccd9c6d43faf44e2646b22cec1
Mejia Ramos Barbara <barbarasofimeji@gmail.com>
2025-06-12 10:15:35 -0600

    Add files via upload

---
+++ Diagrama de clases.py
@@ -0,0 +1,23 @@
+'''
++---------------------------+
+|        JuegoDelGato       |
++---------------------------+
+| - root: Tk                |
+| - turno: str              |
+| - tablero: list           |
+| - botones: list           |
+| - puntaje_x: int          |
+| - puntaje_o: int          |
+| - marco_superior          |
+| - etiqueta_puntaje        |
+| - marco_tablero           |
++---------------------------+
+| + _init_()                |
+| + obtener_texto_puntaje() |
+| + crear_interfaz()        |
+| + manejar_click()         |
+| + verificar_ganador()     |
+| + reiniciar_tablero()     |
+| + mostrar_ganador()       |
++---------------------------+
+'''
